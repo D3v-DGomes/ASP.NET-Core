b52b335c7f2a777d912397d8f55eb1be4a8907b7
D3v-DGomes <devgomes1223@hotmail.com>
2025-05-22 17:41:08 -0300

    adding layout and script to the register tab and creating a DTO

---
+++ Projects/Pizzaria/Lanchonete/DTO/PizzaCriacaoDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Pizzaria.DTO
+{
+    public class PizzaCriacaoDto
+    {
+        // Criação de uma pizza
+        public int Id { get; set; }
+
+        public string Sabor { get; set; } = string.Empty; 
+
+        public string Capa { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public double Valor { get; set; }
+    }
+}
